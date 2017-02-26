d13ec3cb2d8c86dcf7d8c8818a12b60f9ec51e27
Smati Tungkitkancharoen <stungkit@gmail.com>
2017-02-25 18:12:24 -0800

    Completed Faking Our Call.

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,8 +2,9 @@ var React = require('react');
 
 var WeatherMessage = React.createClass({
     render: function() {
+      var {temp, location} = this.props;
       return (
-        <h3>It is 40 degrees in Philadelphia.</h3>
+        <h3>It is {temp} degrees in {location}.</h3>
       );
     }
 });
